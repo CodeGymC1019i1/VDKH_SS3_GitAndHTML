f4da7c67578d60e4d30b8dedbfd770f6da6c6782
dong94vk <quangdong.071094@gmail.com>
2019-11-05 17:15:03 +0700

    update 02-11-2019

--- [Bai_tap]_VDKH/Đông/chuyen doi tien te/convertUnitOfLengthMeasurement.js
+++
@@ -1,9 +0,0 @@
-function convertUnit() {
-    let result = 0;
-    let length = document.getElementById("length").value;
-    let fromUnit = document.getElementById("fromUnit").value;
-    let toUnit = document.getElementById("toUnit").value;
-
-    result = (length*toUnit)/fromUnit;
-    document.getElementById("result").innerHTML = result.toString();
-}
--- [Bai_tap]_VDKH/Đông/tinh diem ly hoa sinh/tinh_diem.js
+++
@@ -1,9 +0,0 @@
-var a = parseFloat(prompt("nhập điểm lý"));
-var b = parseFloat(prompt("nhập điểm hóa"));
-var c = parseFloat(prompt("nhập điểm sinh"));
-function total() {
-    document.write(a+b+c + "<br>");
-}
-function average() {
-    document.write((a+b+c)/3 + "<br>");
-}
